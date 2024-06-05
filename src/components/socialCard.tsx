e428858bed88aac0152bd37ca92a126a1c9c19e6
Martin Cooper <Martin.Cooper@ao.com>
2024-06-05 13:50:30 +0100

    Great new divs

---
+++ src/components/socialCard.tsx
@@ -0,0 +1,24 @@
+function SocialCard() {
+
+  
+    return (
+      <>
+        <div>
+        User
+        </div>
+        
+        <div>
+        Image
+        </div>
+        
+        <div>
+            Message
+        </div>
+        <div>
+            Reaction buttons
+        </div>
+      </>
+    )
+  }
+  
+  export default SocialCard
