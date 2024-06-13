38b4afd687f60c7662c1f6a7cd805d3eb568cacb
Martin Cooper <Martin.Cooper@ao.com>
2024-06-13 06:41:14 +0100

    Update content

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,33 @@ import portrait from "./components/social-card/portrait.jpg";
 
 
 function App() {
-  const defaultComments = [
+  const defaultComments1 = [
     {
       id: 1,
-      name: 'Alice',
+      name: 'Ryan Robinson',
       time: '2:00pm',
-      body: 'What a cute hedgehog!',
+      body: 'Is that the a first edition of Clean Code?',
     },
     {
       id: 2,
-      name: 'Bob',
+      name: 'Dave Williams (Tech)',
       time: '2:15pm',
-      body: 'I love hedgehogs!',
+      body: 'It\'s The Tale of Mrs. Tiggy-Winkle, my favourite',
+    },
+  ];
+
+  const defaultComments2 = [
+    {
+      id: 1,
+      name: 'Simon Cragg',
+      time: '2:10pm',
+      body: 'You can probably delete half of that',
+    },
+    {
+      id: 1,
+      name: 'Ryan Robinson',
+      time: '2:15pm',
+      body: 'Maybe you could rewrite it in dotnet while you\'re at it?',
     },
   ];
 
@@ -30,7 +45,7 @@ function App() {
         surname="Williams (Tech)"
         mainImage="https://t3.ftcdn.net/jpg/05/53/71/56/360_F_553715658_13GcX5js3knq0jaLUxw1HglMF7m1KB3q.jpg"
         time="11:00pm"
-        comments={defaultComments}
+        comments={defaultComments1}
       />
       <SocialCard
         postId='2'
@@ -41,7 +56,7 @@ function App() {
         surname="Powers"
         mainImage="https://miro.medium.com/v2/resize:fit:1200/1*mCMY-LDuqcIFr8wjzIZfxw.png"
         time="1:45pm"
-        comments={defaultComments}
+        comments={defaultComments2}
       />
     </div>
   );
